54b44893ab90d2a195ba7e6d71cd93fc0357207c
unknown <shahodatusmonjonova@gmail.com>
2023-12-22 21:22:47 +0500

    first commit

---
+++ js/mein.js
@@ -0,0 +1,24 @@
+function toggleNavbar() {
+  document.getElementById('navbar-responsive').classList.toggle('open');
+}
+
+document.getElementById('menu-btn').addEventListener('click', toggleNavbar);
+
+
+
+window.addEventListener('scroll', function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById('backtop');
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = '20px';
+  } else {
+    backtop.style.bottom = '-50px';
+  }
+}
